24597e53987cb8bb6c4c30be5e44bfb589bad72b
Herry <a0938039592@gmail.com>
2021-12-09 00:18:22 +0800

    使用function重構

--- Products.py
+++ Products.py
@@ -4,46 +4,59 @@ Created on Sat Dec  4 16:14:24 2021
 
 @author: DSC Handsome
 """
-
-
 import os # os系統
 
-Products=[]
-if os.path.isfile('products.txt'):  #檢查檔案在不在
-    print ("找到檔案")
-    
 #讀取檔案
-    with open('products.txt','r', encoding='utf-8') as f:
+
+def read_file(filename):
+    products=[]
+    with open(filename,'r', encoding='utf-8') as f:
         for line in f:
-            if '商品,價格' in line:
+            if '商品,價格' in line:  #把商品及價格這一欄不要顯示出來，跳過此次迴圈
                 continue   #
-                name,price = line.strip().split(',')        #strip為去除換行符號，split用逗號分割
-                Products.append([name,price])
-    print(Products)
-        
-    
-else:
-     print("找不到檔案....")
-     
+            name,price = line.strip().split(',')        #strip為去除換行符號，split用逗號分割
+            products.append([name,price])
+    return products
+
 #讓使用者輸入
-while True:
-    name = input('請輸入商品名稱')
-    if name == 'q':
-        break
-    price = input('請輸入商品價格')
-    
-    p=[name,price]
-    Products.append(p)
+def usr_input(products):
+    while True:
+        name = input('請輸入商品名稱：')
+        if name == 'q':
+            break
+        price = input('請輸入商品價格：')
+        price = int(price)
+        products.append([name,price])
+    print(products)
+    return products
     
 #印出商品價格
-for p in Products:
-    print(p[0], '的價格是', p[1])
+def print_products(products):
+    for p in products:
+        print(p[0], '的價格是', p[1])
     
 #將商品資訊寫入檔案
-with open('products.txt','w', encoding='utf-8') as f:
-    f.write('商品,價格 \n')
-    for p in Products:
-        f.write(p[0] +',' + p[1] + '\n')
+def wirte_file(filename,products):
+    with open(filename,'w', encoding='utf-8') as f:
+        f.write('商品,價格 \n')
+        for p in products:
+            f.write(p[0] +',' + str (p[1]) + '\n')
+            
+            
+def main():
+    filename = 'products.txt'
+    if os.path.isfile(filename):  #檢查檔案在不在
+        print ("找到檔案了!!")
+        products = read_file(filename)
+        print(products)
+    else:
+        print("找不到檔案....")
         
-print(Products)
-print(Products[0])
+    products = usr_input(products)
+    print_products(products)
+    wirte_file('products.txt',products)
+
+main()
+
+
+
